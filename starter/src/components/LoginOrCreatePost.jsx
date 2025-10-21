9b80f84b621f2d249f784ea1119800f0afab1a7f
htreinen <htreinen@wisc.edu>
2025-10-20 23:07:16 -0500

    thru step 6

--- starter/src/components/LoginOrCreatePost.jsx
+++ starter/src/components/LoginOrCreatePost.jsx
@@ -6,17 +6,62 @@ export default function LoginOrCreatePost(props) {
     // Note! You should use this in combination with sessionStorage.
     // Otherwise, when the user refreshes the page, it will go away!
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const usernameRef = useRef();
+    const passwordRef = useRef();
 
     function handleLoginSubmit(e) {
         e?.preventDefault();  // prevents default form submit action
 
         // TODO: POST to https://cs571api.cs.wisc.edu/rest/f25/ice/login
+        fetch('https://cs571api.cs.wisc.edu/rest/f25/ice/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId(), // replace with your actual ID
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "username": usernameRef.current.value,
+                "password": passwordRef.current.value
+            })
+        })
+        .then(res => {
+            if (res.status === 401) {
+                alert("Check credentials");
+            }
+            else {
+                alert("Login successful");
+                setIsLoggedIn(true);
+            }
+        })
     }
 
     function handleCommentSubmit(e) {
         e?.preventDefault(); // prevents default form submit action
         
         // TODO: POST to https://cs571api.cs.wisc.edu/rest/f25/ice/comments
+        fetch('https://cs571api.cs.wisc.edu/rest/f25/ice/comments', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId(), // replace with your actual ID
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "comment": document.getElementById("commentInput").value
+            })
+        })
+        .then(res => {
+            if (res.status === 401) {
+                alert("You must be logged in to post a comment");
+                setIsLoggedIn(false);
+            }
+            else if (res.status === 201) {
+                alert("Comment posted successfully");
+                props.refreshComments(); // Notify parent to refresh comments
+            }
+        })
+        props.refreshComments();
     }
 
     function handleLogout() {
@@ -36,9 +81,9 @@ export default function LoginOrCreatePost(props) {
     } else {
         return <Form onSubmit={handleLoginSubmit}>
             <Form.Label htmlFor="usernameInput">Username</Form.Label>
-            <Form.Control id="usernameInput"></Form.Control>
+            <Form.Control id="usernameInput" ref={usernameRef}></Form.Control>
             <Form.Label htmlFor="passwordInput">Password</Form.Label>
-            <Form.Control id="passwordInput" type="password"></Form.Control>
+            <Form.Control id="passwordInput" type="password" ref={passwordRef}></Form.Control>
             <br/>
             <Button type="submit" onClick={handleLoginSubmit}>Login</Button>
         </Form>
